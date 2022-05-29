93b24ccb3467e080ede669b69d426bf7aca67036
sdellsim <105978479+sdellsim@users.noreply.github.com>
2022-05-29 23:15:28 +1200

    product modal added

---
+++ js/js.js
@@ -0,0 +1,18 @@
+// quantity input from https://codepen.io/rkoms/pen/OJbrGKX
+
+$(document).ready(function() {
+    $('.minus').click(function () {
+        var $input = $(this).parent().find('input');
+        var count = parseInt($input.val()) - 1;
+        count = count < 0 ? 0 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    });
+    $('.plus').click(function () {
+        var $input = $(this).parent().find('input');
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;
+    });
+});
