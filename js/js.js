066c58ede50260f334054c04ef8603335cbc2796
sdellsim <105978479+sdellsim@users.noreply.github.com>
2022-06-04 01:49:07 +1200

    addedtocart.html added

--- js/js.js
+++ js/js.js
@@ -27,3 +27,67 @@ function openNav() {
   function closeNav() {
     document.getElementById("mobileMenu").style.width = "0";
   }
+
+
+// make addtocart.html#cartModal display the cart modal
+
+$(document).ready(function() {
+
+    if(window.location.href.indexOf('#cartModal') != -1) {
+      $('#cartModal').modal('show');
+    }
+
+    else if(window.location.href.indexOf('#signuploginModal') != -1) {
+      $('#signuploginModal').modal('show');
+    }
+
+
+
+    
+    });
+
+// addedtocart.html add to cart button 
+
+
+function addtocart() {
+  location.href='#cart';
+  $('#productModal').modal('hide');
+  document.getElementById('filledcart').style.display = 'block';
+  document.getElementById('emptycart').style.display = 'none';
+
+  document.getElementById('filledcart-modal').style.display = 'block';
+  document.getElementById('emptycart-modal').style.display = 'none';
+  document.getElementById('checkoutbn-modal').style.display = 'block';
+  document.getElementById('calloutitemadded').style.display = 'block';
+
+ 
+
+
+
+
+
+  if ($(window).width() < 664) {
+    document.getElementById('mobile-viewcart-bar').style.visibility = 'visible'; }
+    else {
+      document.getElementById('mobile-viewcart-bar').style.visibility = 'hidden';}
+    
+
+
+}
+
+// delete cart item
+
+function deletecartitem()  {
+document.getElementById('filledcart').style.display = 'none';
+document.getElementById('emptycart').style.display = 'block';
+
+document.getElementById('filledcart-modal').style.display = 'none';
+document.getElementById('emptycart-modal').style.display = 'block';
+document.getElementById('checkoutbn-modal').style.display = 'none';
+
+document.getElementById('mobile-viewcart-bar').style.visibility = 'hidden';
+
+}
+
+
+
