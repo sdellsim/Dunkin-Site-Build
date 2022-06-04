8a7bf0fb2a51e5bacef89e49f9d308beee21ae7e
sdellsim <105978479+sdellsim@users.noreply.github.com>
2022-06-04 15:17:56 +1200

    modal in middle of page

--- js/js.js
+++ js/js.js
@@ -50,8 +50,7 @@ $(document).ready(function() {
 
 
 function addtocart() {
-  location.href='#cart';
-  $('#productModal').modal('hide');
+  
   document.getElementById('filledcart').style.display = 'block';
   document.getElementById('emptycart').style.display = 'none';
 
@@ -60,18 +59,14 @@ function addtocart() {
   document.getElementById('checkoutbn-modal').style.display = 'block';
   document.getElementById('calloutitemadded').style.display = 'block';
 
- 
-
-
-
-
 
   if ($(window).width() < 664) {
-    document.getElementById('mobile-viewcart-bar').style.visibility = 'visible'; }
+    document.getElementById('mobile-viewcart-bar').style.visibility = 'visible';}
     else {
       document.getElementById('mobile-viewcart-bar').style.visibility = 'hidden';}
     
-
+      location.href='#cart';
+      $('#productModal').modal('hide');
 
 }
 
@@ -91,3 +86,39 @@ document.getElementById('mobile-viewcart-bar').style.visibility = 'hidden';
 
 
 
+// vertival align modal https://www.tutorialrepublic.com/codelab.php?topic=faq&file=vertically-centering-bootstrap-modal-window
+
+$(document).ready(function(){
+  function alignModal(){
+      var modalDialog = $(this).find(".modal-dialog");
+      
+      // Applying the top margin on modal to align it vertically center
+      modalDialog.css("margin-top", Math.max(0, ($(window).height() - modalDialog.height()) / 2));
+  }
+  // Align modal when it is displayed
+  $(".modal").on("shown.bs.modal", alignModal);
+  
+  // Align modal when user resize the window
+  $(window).on("resize", function(){
+      $(".modal:visible").each(alignModal);
+  });   
+});
+
+
+// show mobile nav callout div only once per session https://itecnote.com/tecnote/jquery-stop-div-from-showing-more-than-once-per-session-or-visit/
+
+//On user coming to page:
+//Check localStorage on each page visit, if it's null, show your div
+if ((!localStorage.getItem("visited")) && ($(window).width() < 963))  {
+  //show the div
+  document.getElementById("calloutmobilenav").style.display = 'block'; 
+
+  //Set the key
+  localStorage.setItem("visited", "true");
+
+}
+
+//clear localStorage on tab close
+window.onbeforeunload = function() {
+  localStorage.removeItem("visited");
+};
